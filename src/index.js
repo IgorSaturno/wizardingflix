b045ac492fc822adce8cb781a7884e6729912e30
IgorSaturno <igornascimentosaturnino11@gmail.com>
2020-08-18 02:18:42 -0300

    subindo arquivos

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Home from './pages/Home/index.js';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video';
+import CadastroCategoria from './pages/cadastro/Categoria';
+
+//Desafio
+const Pagina404 = () => (<div>Página 404</div>)
+
+ReactDOM.render(
+  <BrowserRouter>
+  <Switch>
+    <Route path="/" component={Home} exact />
+    <Route path="/cadastro/video" component={CadastroVideo} />
+    <Route path="/cadastro/categoria" component={CadastroCategoria} />
+    <Route path="/" component={Pagina404} />
+  </Switch>
+  </BrowserRouter>,
+  document.getElementById('root')
+);
+
+
