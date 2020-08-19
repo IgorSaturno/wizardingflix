c5b1922de8d1b4d3bda3257925b3f9c85ddd2c68
IgorSaturno <igornascimentosaturnino11@gmail.com>
2020-08-18 23:05:11 -0300

    arrumando o server

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -21,7 +21,7 @@ function CadastroCategoria() {
   useEffect(() => {
     const URL_TOP = window.location.hostname.includes('localhost') 
     ? 'http://localhost:8080/categorias'
-    : 'https://saturnooflix.herokuapp.com/categorias';
+    : 'https://wizardingflix.herokuapp.com/categorias';
     fetch(URL_TOP)
       .then(async (respostaDoServidor) => {
         const resposta = await respostaDoServidor.json();
